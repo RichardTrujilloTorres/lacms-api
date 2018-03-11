4d58dbb08e137f8de946087627b5382df981cb1e
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-11 18:04:28 +0100

    Add post show and post store validation

--- app/Http/Controllers/Api/V1/PostsController.php
+++ app/Http/Controllers/Api/V1/PostsController.php
@@ -2,7 +2,38 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+use App\Models\Author;
+
+use Validator;
+
 class PostsController extends BaseController
 {
     protected $model = \App\Models\Post::class;
+
+
+    /**
+     * Get post by ID.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) 
+    {
+        $post = Post::with('author')->findOrFail($id);
+
+        return $post;
+    }
+
+
+
+    protected function validator(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'author_id' => 'required|integer|exists:authors,id',
+            'title' => 'required|unique:posts|max:255',
+        ]);
+    }
 }
