da1f3758d6649e727d50a16a82ca77da6703f9b3
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-16 09:06:34 +0100

    Add images model

---
+++ app/Models/Image.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Author;
+
+class Image extends Model 
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        //
+    ];
+
+}
