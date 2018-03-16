1221c0f38a2d097183a80c795f7917eda95b6d04
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-16 09:30:40 +0100

    Set fillable properties and posts relationship

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,9 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-use App\Models\Author;
-
-class Image extends Model 
+class Image extends Model
 {
     /**
      * The attributes that are mass assignable.
@@ -14,7 +12,11 @@ class Image extends Model
      * @var array
      */
     protected $fillable = [
-        //
+        'url', 'slug', 'post_id',
     ];
 
+    public function posts()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
