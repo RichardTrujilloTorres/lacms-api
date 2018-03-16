41023b09c3f5576f63298bbe4e081d7c41a07120
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-16 09:06:21 +0100

    Add images controller

---
+++ app/Http/Controllers/Api/V1/ImagesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+use App\Models\Author;
+use App\Models\Image;
+
+use Validator;
+
+class ImagesController extends BaseController
+{
+    protected $model = \App\Models\Image::class;
+
+    /**
+     * Get store validator.
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Validation\Validator
+     */
+    protected function validator(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'first_name' => 'required|alpha|max:255',
+            'last_name' => 'required|alpha|max:255',
+            'about' => 'required|max:255',
+            // 
+        ]);
+    }
+}
