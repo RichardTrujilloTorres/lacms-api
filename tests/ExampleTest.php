1f532330c7e55ce0bb251175fdf0ef563a70fb1c
Richard Trujillo <richard.trujillo.torres@gmail.com>
2018-03-19 17:39:28 +0100

    Add temporary test to detect building changes

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,6 +5,17 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
+    /**
+     * Testing building.
+     *
+     * @test 
+     */
+    public function building()
+    {
+        $this->assertEquals(true, true);
+    }
+
     /**
      * A basic test example.
      *
