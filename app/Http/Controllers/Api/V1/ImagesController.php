6e870b4e9543af0bc5f72825e3db9f96fefe52d3
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2018-04-08 00:22:23 +0000

    Add image storage, retrieval and download methods

--- app/Http/Controllers/Api/V1/ImagesController.php
+++ app/Http/Controllers/Api/V1/ImagesController.php
@@ -17,6 +17,103 @@ class ImagesController extends BaseController
 {
     protected $model = \App\Models\Image::class;
 
+    // @todo replace w/ cloud path (url shortener)
+    protected $urlRootPath = '/api/images';
+
+    /**
+     * Download an image by slug.
+     *
+     * @return \Illuminate\Http\Response 
+     */
+    public function downloadBySlug($slug)
+    {
+        $image = Image::where('slug', $slug)->firstOrFail();
+
+        return Storage::disk('local')->download($image->filename);
+    }
+
+    /**
+     * Get an image by slug.
+     *
+     * @return \Illuminate\Http\Response 
+     */
+    public function getBySlug($slug)
+    {
+        $image = Image::where('slug', $slug)->firstOrFail();
+
+        return $this->response->array([
+            'status' => 'success',
+            'message' => '',
+            'data' => [
+                'image' => $image,
+            ],
+        ]);
+    }
+
+
+
+
+
+
+    /**
+     * Store a new image.
+     *
+     * @return \Illuminate\Http\Response 
+     */
+    public function store()
+    {
+        $validator = $this->validator($this->requests);
+        if ($validator->fails()) {
+            throw new \Dingo\Api\Exception\StoreResourceFailedException(
+                'Could not create new resource.',
+                $validator->errors()
+            );
+        }
+
+        if (! $this->requests->hasFile('file')) {
+            return $this->response->array([
+                'status' => 'error',
+                'message' => 'Invalid request',
+                'errors' => ['Invalid file.',],
+            ]);
+        }
+
+        if (! $this->requests->file('file')->isValid()) {
+            return $this->response->array([
+                'status' => 'error',
+                'message' => 'Invalid request',
+                'errors' => ['Invalid file upload.',],
+            ]);
+        }
+
+        $slug = uniqid().'_'.str_slug(explode('.', $this->requests->file->getClientOriginalName())[0]);
+        $filename = $slug.'.'.$this->requests->file->extension();
+
+        // move image
+        $this->requests->file->move(
+            storage_path('app'), 
+            $filename
+        );
+
+        $image = Image::create([
+            'slug' => $slug,
+            'url' => $this->urlRootPath.'/'.$slug,
+            'filename' => $filename,
+            'post_id' => $this->requests->post_id,
+        ]);
+
+        return $this->response->array([
+            'status' => 'success',
+            'message' => 'Image saved.',
+            'data' => [
+                'image' => $image,
+            ],
+        ]);
+
+
+
+    }
+
     /**
      * Get store validator.
      *
